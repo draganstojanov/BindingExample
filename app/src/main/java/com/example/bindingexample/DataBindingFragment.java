c5b1ff61074b45bd2af3b50472aaf52e24c18c5b
draganstojanov <dragan.stojanov@gmail.com>
2020-05-13 15:37:46 +0200

    include

--- app/src/main/java/com/example/bindingexample/DataBindingFragment.java
+++ app/src/main/java/com/example/bindingexample/DataBindingFragment.java
@@ -1,10 +1,11 @@
 package com.example.bindingexample;
 
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.bindingexample.databinding.FragmentDataBindingBinding;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,14 +13,13 @@ import androidx.databinding.ObservableField;
 import androidx.databinding.ObservableInt;
 import androidx.fragment.app.Fragment;
 
-import com.example.bindingexample.databinding.FragmentDataBindingBinding;
-
 public class DataBindingFragment extends Fragment {
 
     FragmentDataBindingBinding binding;
     public ObservableField<String> observableField = new ObservableField<>();
-
     public ObservableInt observableInt = new ObservableInt();
+
+    public boolean isIncludedViewVisible = true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,28 +33,42 @@ public class DataBindingFragment extends Fragment {
         User user = new User("John", "Peters", "Peterburg");
         binding.setUser(user);
         binding.setFragment(this);
+        binding.setIsIncludedViewVisible(isIncludedViewVisible);
     }
 
     public void onButtonClicked() {
         ((MainActivity) getActivity()).getSupportFragmentManager().popBackStack();
     }
 
-    public void onEditTextShowClicked() {
-        binding.showTextTv.setText(observableField.get());
+    public void caseFirst() {
+
+        int visibility = binding.testInclude.includedLayoutCl.getVisibility();
+        ;
+        visibility = (visibility == View.VISIBLE) ? View.GONE : View.VISIBLE;
+
+        binding.testInclude.includedLayoutCl.setVisibility(visibility);
     }
 
+    public void caseSecond() {
+        isIncludedViewVisible = !isIncludedViewVisible;
+        binding.setIsIncludedViewVisible(isIncludedViewVisible);
+    }
 
-    CountDownTimer cdt = new CountDownTimer(10000, 1000) {
-        @Override
-        public void onTick(long millisUntilFinished) {
-            observableInt.set((int) millisUntilFinished / 1000); {
-            }
-        }
+    // public void onEditTextShowClicked() {
+    //     binding.showTextTv.setText(observableField.get());
+    //  }
 
-        @Override
-        public void onFinish() {
-
-        }
-    }.start();
+//    CountDownTimer cdt = new CountDownTimer(10000, 1000) {
+//        @Override
+//        public void onTick(long millisUntilFinished) {
+//            observableInt.set((int) millisUntilFinished / 1000); {
+//            }
+//        }
+//
+//        @Override
+//        public void onFinish() {
+//
+//        }
+//    }.start();
 
 }
